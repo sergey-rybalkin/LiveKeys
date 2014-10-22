9e01de1e0955a2f4634afdd01a5abef2a8c0720b
Sergey Rybalkin <rybalkinsp@gmail.com>
2014-10-22 17:35:14 +0300

    Layout switcher is now using Win+Space shortcut

--- LayoutSwitcher.cpp
+++ LayoutSwitcher.cpp
@@ -94,19 +94,19 @@ VOID CLayoutSwitcher::SetLayout ( DWORD dwLayout )
     // Emulate Win + Space keyboard shortcut that switches languages regardless of the system settings
     INPUT inputs [ 4 ] = { 0 } ;
 
-    inputs[0].type = INPUT_KEYBOARD;
-    inputs[0].ki.wVk = VK_CONTROL;
+    inputs[0].type = INPUT_KEYBOARD ;
+    inputs[0].ki.wVk = VK_LWIN ;
 
-    inputs[1].type = INPUT_KEYBOARD;
-    inputs[1].ki.wVk = VK_SHIFT;
+    inputs[1].type = INPUT_KEYBOARD ;
+    inputs[1].ki.wVk = VK_SPACE ;
 
-    inputs[2].type = INPUT_KEYBOARD;
-    inputs[2].ki.wVk = VK_SHIFT;
-    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+    inputs[2].type = INPUT_KEYBOARD ;
+    inputs[2].ki.wVk = VK_SPACE ;
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP ;
 
-    inputs[3].type = INPUT_KEYBOARD;
-    inputs[3].ki.wVk = VK_CONTROL;
-    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+    inputs[3].type = INPUT_KEYBOARD ;
+    inputs[3].ki.wVk = VK_LWIN ;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP ;
 
     SendInput ( 4 , inputs , sizeof ( INPUT ) ) ;
 }
